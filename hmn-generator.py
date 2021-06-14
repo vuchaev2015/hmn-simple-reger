7d383f7beb6dd340c307e96505f6d85e96733e8b
vuchaev2015 <42405830+vuchaev2015@users.noreply.github.com>
2021-06-14 21:23:49 +0300

    Add files via upload

---
+++ hmn-generator.py
@@ -0,0 +1,33 @@
+import requests
+
+url = 'https://hidemy.name/ru/demo/'
+
+if 'Ваша электронная почта' in requests.get(url).text:
+    
+    email = input('Введите электронную почту для получения тестового периода: ')
+
+    response = requests.post('https://hidemy.name/ru/demo/success/', data={
+        "demo_mail": f"{email}"
+    })
+
+    if 'Ваш код выслан на почту' in response.text:
+        confirm = input('Введите полученную ссылку для подтверждения e-mail адреса: ')
+        
+        while True:
+            try:
+                response = requests.get(confirm)
+                if 'Спасибо' in response.text:
+                    print('Почта подтверждена. Код отправлен на вашу почту!')
+                    break
+                else:
+                    confirm = input('Ссылка невалидная, повторите попытку: ')
+            except:
+                confirm = input('Ссылка невалидная, повторите попытку: ')
+                continue
+
+
+    else:
+        print('Указанная почта не подходит для получения тестового периода ')
+
+else:
+    print('Невозможно получить тестовый период')
